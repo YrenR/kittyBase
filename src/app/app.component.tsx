08241e0147067a9cd86662307b1c8793303f403d
Yren R <yren.rivero@gmail.com>
2022-02-27 16:49:46 +0100

    feat: simplify themes app

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -4,40 +4,20 @@ import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import {AppIconsPack} from './app-icons-pack';
 import {StatusBar} from '../components/status-bar.component';
 import {AppNavigator} from '../navigation/app.navigator';
-import {Mapping, Theme, Theming} from '../services/theme.service';
 import {AppearanceProvider} from 'react-native-appearance';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {appMappings, appThemes} from './app-theming';
-
-const defaultConfig: {mapping: Mapping; theme: Theme} = {
-  mapping: 'eva',
-  theme: 'light',
-};
+import * as eva from '@eva-design/eva';
 
 const App: React.FC = () => {
-  const [mappingContext, currentMapping] = Theming.useMapping(
-    appMappings,
-    defaultConfig.mapping,
-  );
-  const [themeContext, currentTheme] = Theming.useTheming(
-    appThemes,
-    defaultConfig.mapping,
-    defaultConfig.theme,
-  );
-
   return (
     <React.Fragment>
       <IconRegistry icons={[EvaIconsPack, AppIconsPack]} />
       <AppearanceProvider>
-        <ApplicationProvider {...currentMapping} theme={currentTheme}>
-          <Theming.MappingContext.Provider value={mappingContext}>
-            <Theming.ThemeContext.Provider value={themeContext}>
-              <SafeAreaProvider>
-                <StatusBar />
-                <AppNavigator />
-              </SafeAreaProvider>
-            </Theming.ThemeContext.Provider>
-          </Theming.MappingContext.Provider>
+        <ApplicationProvider {...eva} theme={eva.light}>
+          <SafeAreaProvider>
+            <StatusBar animated={true} />
+            <AppNavigator />
+          </SafeAreaProvider>
         </ApplicationProvider>
       </AppearanceProvider>
     </React.Fragment>
--- src/components/status-bar.component.tsx
+++ src/components/status-bar.component.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  StatusBar as RNStatusBar,
-  StatusBarProps as RNStatusBarProps,
-  ViewProps,
-} from 'react-native';
+import {StatusBar as RNStatusBar, StatusBarProps as RNStatusBarProps, ViewProps} from 'react-native';
 import {StyledComponentProps} from '@ui-kitten/components';
 
 export type StatusBarProps = RNStatusBarProps & StyledComponentProps;
